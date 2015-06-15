7b3c377948075c2a414777de3172ff17d021a37b
mbouzi <mrbouzi@gmail.com>
2015-06-15 09:44:51 -0400

    add days amount options

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ weatherApp.config(function ($routeProvider) {
 		templateUrl: 'pages/forecast.html',
 		controller: 'forecastController'
 	})
+	.when('/forecast/:days', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
 
 });
 
@@ -39,9 +43,11 @@ weatherApp.controller('homeController', ['$scope', '$resource','cityService', fu
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
 	$scope.city = cityService.city;
+
+	$scope.days = $routeParams.days || 2;
 
 	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
