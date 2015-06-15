53866a6aedd402e8a1bb95e5d47f6253a8f9f0d0
mbouzi <mrbouzi@gmail.com>
2015-06-14 23:19:55 -0400

    add watch to cityService

--- app.js
+++ app.js
@@ -23,20 +23,25 @@ weatherApp.config(function ($routeProvider) {
 
 weatherApp.service('cityService', function() {
 
-	this.city = "New York, NY"
+	this.city = "New York, NY";
 
 });
 
 
 //CONTROLLERS
-weatherApp.controller('homeController', 'cityService', ['$scope', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope','cityService', function($scope, cityService) {
 
 	$scope.city = cityService.city;
+
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	});
 
 }]);
 
-weatherApp.controller('forecastController', 'cityService', ['$scope', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
 
 	$scope.city = cityService.city;
+
 
 }]);
